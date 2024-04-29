6492e2e2e02fd80bb0dcaebbe13e597e0e28bc06
marcocenturion <aulavirtualturismo@gmail.com>
2024-04-29 19:25:42 -0300

    index con variables

--- proyecto1/saludo/views.py
+++ proyecto1/saludo/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 def saludo(request):
-    return render(request, 'index.html')
+    mensaje = 12 * 2
+    my_context = {
+            "variable1": "Plantilla con contexto",
+            "variable2": str(mensaje)
+            }
+    return render(request,'index.html',my_context)
 
