1abb1be3b26e39cfabbfdfe5b4bf2cf9bfd240a4
marcocenturion <aulavirtualturismo@gmail.com>
2024-04-30 19:03:13 -0300

    royecto django

--- proyecto1/saludo/views.py
+++ proyecto1/saludo/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 
 def saludo(request):
-    mensaje = 12 * 2
-    my_context = {
-            "variable1": "Plantilla con contexto",
-            "variable2": str(mensaje)
-            }
-    return render(request,'index.html',my_context)
+   data = request.POST.get('name')
+   print(data)
+   return render(request,'index.html',{'data':data})
 
